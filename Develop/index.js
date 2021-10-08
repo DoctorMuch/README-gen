f3968efe263daf03e10235a7252551d11347726d
David F Mueth <davidmueth@gmail.com>
2021-10-08 16:36:49 -0500

    working on it, trying to refresh my memory

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,22 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
+const [name, github] = profileDataArgs;
+
+
+const generatePage = (name, github) => {
+  return `
+    # ${name}
+    ## ${github}
+  `;
+};
+
+fs.writeFile('./readme.md', generatePage(name, github), err => {
+  if(err) throw err;
+
+  console.log('Congrats! You just created a readme.md page!');
+})
 // TODO: Create an array of questions for user input
 const questions = [];
 
